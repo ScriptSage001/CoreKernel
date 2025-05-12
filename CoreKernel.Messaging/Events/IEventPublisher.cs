9016fb303a9f2c0b549a7ead9211d170056910e0
ScriptSage001 <ScriptSage001@gmail.com>
2025-05-12 22:49:33 +0530

    Modified csproj of CoreKernel and CoreKernel.Messaging to add support for the nuget library

--- CoreKernel.Messaging/Events/IEventPublisher.cs
+++ CoreKernel.Messaging/Events/IEventPublisher.cs
@@ -1,6 +1,17 @@
+using System.Threading.Tasks;
+
 namespace CoreKernel.Messaging.Events;
 
+/// <summary>
+/// Defines a contract for publishing events.
+/// </summary>
 public interface IEventPublisher
 {
+    /// <summary>
+    /// Publishes an event asynchronously.
+    /// </summary>
+    /// <typeparam name="TEvent">The type of the event to publish.</typeparam>
+    /// <param name="event">The event instance to publish.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     Task PublishAsync<TEvent>(TEvent @event) where TEvent : IEvent;
 }
